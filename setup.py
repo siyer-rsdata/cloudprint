93c200a4e9b2f197fd753bc5312100558514fcfb
siyer-rsdata <sivaram.iyer@rsdataarchitect.com>
2024-03-14 08:45:43 -0400

    minor logger format cleanup.

--- setup.py
+++ setup.py
@@ -21,20 +21,21 @@ def init():
     # Configure logging
     logging.basicConfig(
         # Specify the file to which logs will be written
+        # filename='logs\\cloudprint.log',
         filename=os.getenv('CLOUDPRINT_LOG'),
 
         # Set the logging level to DEBUG to capture all levels of messages
         level=logging.DEBUG,
 
         # Specify the log message format
-        format='%(asctime)s - %(levelname)s - %(message)s'
+        format='%(levelname)s: %(message)s'
     )
 
 
 def create_folder(folder_name: str):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
-        print(f"Folder:{folder_name} created successfully.")
+        logging.info(f"Folder:{folder_name} created successfully.")
     else:
-        print(f"Folder:{folder_name} already exists.")
+        logging.info(f"Folder:{folder_name} already exists.")
 
